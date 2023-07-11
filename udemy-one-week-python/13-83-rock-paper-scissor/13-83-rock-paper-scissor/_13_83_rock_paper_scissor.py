8635490581331ca8443f91619b64e270b9a14eb5
Neil Ranada <neil.ranada@gmail.com>
2023-07-10 19:41:20 -0700

    Solution attempt

--- udemy-one-week-python/13-83-rock-paper-scissor/13-83-rock-paper-scissor/_13_83_rock_paper_scissor.py
+++ udemy-one-week-python/13-83-rock-paper-scissor/13-83-rock-paper-scissor/_13_83_rock_paper_scissor.py
@@ -29,16 +29,47 @@ scissors = """
 num = randint(1,3)
 
 # Turn that random number into the computer's RPS move
+def getComputerMove(num):
+    if num == 1: return rock
+    if num == 2: return paper
+    if num == 3: return scissors
+
+computer_move = getComputerMove(num)
 
 
 # Ask a user to enter their move
-
+user_entry = input("What is your move? ")
 
 # Print the rock, paper, or scissors ASCII art that corresponds to the player's move
+def getUserMove(user_entry):
+    if user_entry == "rock": return rock
+    if user_entry == "paper": return paper
+    if user_entry == "scissors": return scissors
 
+user_move = getUserMove(user_entry)
+
+print("Your move:")
+print(user_move)
+print()
 
 # Print the rock, paper, or scissors ASCII art that corresponds to the computer's move
+print("Computer's move:")
+print(computer_move)
+print()
 
 
 # Figure out who wins and print the result!
+if user_move == rock:
+    if computer_move == rock: print("Draw!")
+    if computer_move == paper: print("Computer wins!")
+    if computer_move == scissors: print("You win!")
 
+if user_move == paper:
+    if computer_move == rock: print("You win!")
+    if computer_move == paper: print("Draw!")
+    if computer_move == scissors: print("Computer wins!")
+
+if user_move == scissors:
+    if computer_move == rock: print("Computer wins!")
+    if computer_move == paper: print("You win!")
+    if computer_move == scissors: print("Draw!")
