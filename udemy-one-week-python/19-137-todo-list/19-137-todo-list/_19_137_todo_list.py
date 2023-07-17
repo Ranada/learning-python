4319e8c35543a2bfcbaa3bbc9338a1b03169404d
Neil Ranada <neil.ranada@gmail.com>
2023-07-17 14:13:01 -0500

    handle completed task and help menu

--- udemy-one-week-python/19-137-todo-list/19-137-todo-list/_19_137_todo_list.py
+++ udemy-one-week-python/19-137-todo-list/19-137-todo-list/_19_137_todo_list.py
@@ -11,26 +11,44 @@ def print_header():
 def print_todos(todos):
     for i, todo in enumerate(todos):
         print(f"{i + 1}) {todo}")
+    print()
 
-def print_completed(completed_todos):
-    print(f"You completed {len(completed_todos)} tasks.")
-    for todo in completed_todos:
+def print_completed(completed):
+    print(f"You completed {len(completed)} tasks:")
+    for todo in completed:
         print(f"* {todo}")
+    print()
+
+def print_help_menu():
+    menu = """
+TODO LIST HELP
+Type 'q' to quit
+To add a todo to the list, type it and hit enter
+To complete a todo enter its number
+"""
+    print(menu)
 
 def create_todos():
     todos = []
-    completed_todos = []
+    completed = []
     divider = "*" * 100
-    print(divider)
     entry = ''
 
     while entry != 'q' or entry != 'quit':
+        print(divider)
         entry = input("Enter a command. Type 'h' for help:\n")
-        if entry == 'q' or entry == 'quit':
-            print("Print completed todos here.")
-            print_completed(completed_todos)
+        if ord(entry[0]) >= ord('0') and ord(entry[0]) <= ord('9'):
+            num = int(entry) - 1
+            task = todos.pop(num)
+            completed.append(task)
+        elif entry == 'h':
+            print_help_menu()
+            continue
+        elif entry == 'q' or entry == 'quit':
+            print_completed(completed)
             return
-        todos.append(entry)
+        else:
+            todos.append(entry)
         print_todos(todos)
 
 print_header();
