2ce0b9cc12d8356e2a81a3f1db47c8a908d11e56
Neil Ranada <neil.ranada@gmail.com>
2023-07-18 14:39:12 -0400

    Peak dictionary solution update

--- udemy-one-week-python/20-149-peak-dictionary/20-149-peak-dictionary/_20_149_peak_dictionary.py
+++ udemy-one-week-python/20-149-peak-dictionary/20-149-peak-dictionary/_20_149_peak_dictionary.py
@@ -10,32 +10,45 @@ peak = {
 }
 # Without touching the original variable declaration (above)...
 # Add a "range" key to peak and set it equal to "Elk Mountains"
+peak["range"] = "Elk Mountains"
 
 # Add a "first_climbed" key to peak and set it equal to 1873
+peak["first_climbed"] = 1873
 
 # Whoops, there's a mistake with the peak "height".  Update it to 14265
+peak["height"] = 14265
 
 # Add a "Verizon" to the "cell_reception" dict and set it equal to "good"
+peak["cell_reception"]["Verizon"] = "good"
 
 # You just summited the peak! Add your name to the "summit_log" list
-
+peak["summit_log"].append("Neil")
 
 # Let's rename "height" to "elevation":
 # Remove "height" from the dict and store the result in a variable.
-
+elevation = peak["height"]
 
 # Use the value for "height" and store it in the dict under they key "elevation"
+peak["elevation"] = elevation
 
 
 # Loop over the values in the dictionary and print them all out.  Don't ask why, just do it :) 
+print("Loop through KEYS")
+for value in peak.values():
+    print(value)
 
 
 # Loop over the keys AND values in the dictionary and print them all out in the following format:
 # key -> value
 # (print an arrow between each pair)
-
+print("Loop through KEYS and VALUES")
+for k, v in peak.items():
+    print(k, "->", v)
 
 # A huge earthquake/meteor/forestfire/tsunami has destroyed the peak.  Please empty out the entire dictionary.
+peak.clear()
+
+print("\nPeak dictionary:\n", peak)
 
 
 
